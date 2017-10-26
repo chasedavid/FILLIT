73b1e37f82b961227d5951163b0a16e5c0d2bdeb
Annie Ho <aho@e1z2r8p11.42.us.org>
2017-10-26 16:42:27 -0700

    added makemap function

--- fillit.c
+++ fillit.c
@@ -1,13 +1,45 @@
 #include "libft.h" 
 #include <stdio.h>
 
+char *makemap(int n)
+{
+	int maplen;
+	int i; 
+	char *map;
+	
+	i = 0;
+	maplen = (n * n) + n + 1;
+	map = ft_memalloc(maplen);
+	map[maplength] = '\0';
+	while (i < (maplen - 1))
+	{
+		if (((i + 1) % (n + 1)) == 0 ) 
+			map[i] = '\n';
+		else
+			map[i] = '.';
+		i++;
+	}
+	return (map); 
+}
+
+
+
+
+
+
 void fillit(t_list **alst)
 {
-	char *grid;
+	char *map;
+	int mapsize;
 	t_list *current; //this is the current piece
 
-	grid = ft_memalloc(7); //mallocs space for 2x2 (6 chars + null terminator)
-	grid = "..\n..\n"; //initialize grid
+	mapsize = 2;
+	map = makemap(mapsize);
+
+	printf("map:\n%s \n", map);
+
+//	grid = ft_memalloc(7); //mallocs space for 2x2 (6 chars + null terminator)
+//	grid = "..\n..\n"; //initialize grid
 
 //	printf("%s", grid);
 
@@ -16,7 +48,7 @@ void fillit(t_list **alst)
 	current = *alst;
 
 	// 0. iterate through linked list to test each piece
-	ft_lstiter(*alst, grid, checkpiece);
+	ft_lstiter(*alst, map, checkpiece);
 
 
 	//once all pieces have been placed, print grid to show solution
