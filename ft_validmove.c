566ec7962d4ad46aee86b87a89fcef36f9e94a4e
Annie Ho <aho@e1z2r8p11.42.us.org>
2017-10-25 19:02:07 -0700

    annie's branch with grid filling functions

---
+++ ft_validmove.c
@@ -0,0 +1,19 @@
+#include "libft.h" 
+
+//this function checks if the data in link passed in can fit into the grid
+
+int placepiece(t_list *link, char *grid, char counter)
+{
+	char *piece; 
+	piece = trimpiece(link);
+
+	//change available spaces in grid to A
+	//change piece to A
+	//if there's a match, place into grid 
+
+
+
+
+
+	return (1);
+}
