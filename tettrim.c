947c10d8961ebc4562125a99b27381ac75cbe3b6
Annie Ho <aho@e1z2r8p11.42.us.org>
2017-10-27 01:19:37 -0700

    added functions to trim tetrimino piece to place into grid

--- tettrim.c
+++ tettrim.c
@@ -1,19 +1,67 @@
 #include "libft.h"
 #include <stdio.h>
 
+int checktetcol(char **array)
+{
+	int i; 
+	int j;
+	int counter;
 
-void checktetcol(char *row)
+	i = 0;
+	j = 0;
+	counter = 0;
+	while (i < 4)
+	{
+		if (array[i][j] == '.') 
+		{
+			counter++;
+			if (counter == 4)
+				return (j);
+			i++;
+		}
+		else if (array[i][j] != '.') 
+		{
+			j++;
+			i = 0;
+			counter = 0;
+		}
+		if (j > 4) 
+			return(-1);
+	}
+	return (-1);
+}
+
+char *strtrimi(char *str, int index)
+{
+	int i;
+	int j;
+	char *trim; 
+	trim = ft_memalloc(ft_strlen(str));
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (i == index) 
+			j++;
+		trim[i] = str[j];
+		i++; 
+		j++; 
+	}
+	return (trim);
+}
+
+
+char **tettrimcol(char **array, int index)
 {
 	int i; 
 
 	i = 0; 
-	while (i < 4)
+	while (i < 4) 
 	{
-		if ()
-		{
-		}			
+		array[i] = strtrimi(array[i], index);
+		i++;
 	}
-
+	return (array);
 }
 
 
@@ -21,12 +69,30 @@ void tettrim(char *str)
 {
 	char **tet2d;
 	int i; 
+	int trimcol;
 
 	tet2d = ft_strsplit(str, '\n');
+
 	i = 0; 
 	while (i < 4)
 	{
 		printf("tet2d[%i]: %s \n", i, tet2d[i]);
+		i++;
+	}
+
+
+	trimcol = checktetcol(tet2d);
+	while (trimcol >= 0)
+	{
+		tettrimcol(tet2d, trimcol);
+		trimcol = checktetcol(tet2d);
+	}
+	
+
+	i = 0; 
+	while (i < 4)
+	{
+		printf("after trim - tet2d[%i]: %s \n", i, tet2d[i]);
 		i++;
 	}
 
