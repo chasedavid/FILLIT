a85bd5d66d084edd18796a19cef913a412bc0928
Annie Ho <girlybit@gmail.com>
2017-11-23 23:25:10 -0800

    adding work in progress files - validate-wip-ah.c and validatechars-wip-ah.c

---
+++ validate-wip-ah.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aho <aho@student.42.us.org>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/15 12:26:45 by aho               #+#    #+#             */
+/*   Updated: 2017/11/23 23:24:22 by aho              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+int			hashrightcheck(char *buf, int i)
+{
+	if (buf[i + 1] == '#')
+		return (1);
+	return (-1);
+}
+
+int			hashleftcheck(char *buf, int i)
+{
+	if (buf[i - 1] == '#')
+		return (1);
+	return (-1);
+}
+
+int			hashbelowcheck(char *buf, int i)
+{
+	if (buf[i + 5] == '#')
+		return (1);
+	return (-1);
+}
+
+int			hashneighborcheck(char *buf, int i, int hashes)
+{
+	if (hashes > 2)
+		return (1);
+	if (hashrightcheck(buf, i) == 1)
+		return (1);
+	if (hashbelowcheck(buf, i) == 1)
+		return (1);
+	if (hashleftcheck(buf, i) == 1)
+		return (1);
+	return (-1);
+}
+
+int			hashchecks(char *buf, int hashes)
+{
+	spaces = 4;
+	hashes++;
+	if (hashes > 4)
+		return (-1);
+	hashneigborcheck(buf, i, hashes);
+	return (1);
+}
+
+
+int			validhashes(char *buf)
+{
+	int		i;
+	int		spaces;
+	int		hashes;
+
+	i = 0;
+	hashes = 0;
+	while (buf[i] != '\0')
+	{
+		while (buf[i] != '#' && hashes == 0)
+		{
+			i++;
+			if (buf[i] == '\0')
+				return (-1);
+		}
+		if (buf[i] == '#')
+		{
+			if (hashchecks(buf, &hashes, &spaces) == -1)
+				return (-1);
+			hashchecks(buf, &hashes);
+		}
+		if (buf[i] != '#' && buf[i] != '\0' && hashes < 4)
+		{
+			if (spaces == 0)
+				return (-1);
+			spaces--;
+		}
+		i++;
+	}
+	return (1);
+}
---
+++ validatechars-wip-ah.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate-wip-ah.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aho <aho@student.42.us.org>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/23 23:17:35 by aho               #+#    #+#             */
+/*   Updated: 2017/11/23 23:17:36 by aho              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+int			checkchars(char *buf)
+{
+	if (buf[4] != '\n' || buf[9] != '\n' || buf[14] != '\n' || buf[19] != '\n')
+		return (-1);
+	if (buf[i] != '.' && buf[i] != '#' && buf[i] != '\n')
+		return (-1);
+	return (1);
+}
+
+int			validchars(char *buf)
+{
+	int i;
+	int dots;
+	int hashes;
+	int newlines;
+
+	i = -1;
+	dots = 0;
+	hashes = 0;
+	newlines = 0;
+	while (buf[++i] != '\0')
+	{
+		if (checkchars(buf) == -1)
+			return (-1);
+		if (dots > 12 || hashes > 4 || newlines > 4)
+			return (-1);
+		if (buf[i] == '.')
+			dots++;
+		if (buf[i] == '#')
+			hashes++;
+		if (buf[i] == '\n')
+			newlines++;
+	}
+	if (dots != 12 || hashes != 4 || newlines != 4)
+		return (-1);
+	return (1);
+}
