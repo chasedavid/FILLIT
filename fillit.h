a631ddf97629d374cc75f8f37c1c6ca308d3287c
Annie Ho <aho@e1z2r8p11.42.us.org>
2017-11-23 19:50:56 -0800

    norm compliant fillit.h

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: cfarnswo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 15:37:24 by cfarnswo          #+#    #+#             */
-/*   Updated: 2017/11/23 19:03:38 by cfarnswo         ###   ########.fr       */
+/*   Updated: 2017/11/23 19:50:33 by aho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 typedef struct		s_tet
 {
 	char			*content;
-	unsigned int	content_size;
+	int				content_size;
 	int				alpha;
 	int				x;
 	int				y;
@@ -24,33 +24,32 @@ typedef struct		s_tet
 	struct s_tet	*next;
 }					t_tet;
 
-typedef struct		s_map	
+typedef struct		s_map
 {
 	char			**grid;
 	int				size;
 }					t_map;
 
-int		readtetfile(char *argv, t_tet **list);
-t_tet	*ft_tetnew(void const *content, unsigned int content_size, int alpha, int list_i);
-void	ft_tetappend(t_tet **alst, t_tet *new);
-void 	ft_printtetlist(t_tet **alst);
-int		validhashes(char *buf);
-int     validchars(char *buf);
-char	*ft_strsub_free(char *str, int col, int n);
-void	trimpieces(t_tet *list);
-char	*tettrim(char *str);
-char    *t_deletecol(char *str, int col, int n);
-char	*trimcols(char *str);
-char	*trimrows(char *str);
-//char	*tetrim(char *str);
-char	**makemap(void);
-void	printmap(char **map, int size);
-int		ft_width(char *s);
-int		ft_height(char *s);
-int		ft_sqrt(int n);
-void	remove_tet(t_map *map, t_tet *tet, int row, int col);
-void	fillit(t_map *map, t_tet *tet);
-int		place_tet(t_map *map, t_tet *tet, int row, int col);
-int		place_on_map(t_map *map, t_tet *tet);
+int					readtetfile(char *argv, t_tet **list);
+t_tet				*ft_tetnew(void *content, int tetsize, int alpha, int i);
+void				ft_tetappend(t_tet **alst, t_tet *new);
+void				ft_printtetlist(t_tet **alst);
+int					validhashes(char *buf);
+int					validchars(char *buf);
+char				*ft_strsub_free(char *str, int col, int n);
+void				trimpieces(t_tet *list);
+char				*tettrim(char *str);
+char				*t_deletecol(char *str, int col, int n);
+char				*trimcols(char *str);
+char				*trimrows(char *str);
+char				**makemap(void);
+void				printmap(char **map, int size);
+int					ft_width(char *s);
+int					ft_height(char *s);
+int					ft_sqrt(int n);
+void				remove_tet(t_map *map, t_tet *tet, int row, int col);
+void				fillit(t_map *map, t_tet *tet);
+int					place_tet(t_map *map, t_tet *tet, int row, int col);
+int					place_on_map(t_map *map, t_tet *tet);
 
 #endif
