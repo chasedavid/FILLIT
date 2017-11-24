d4cdf1d044d5dd8986b238d7171e4770da5c3548
chasedavid <chasedavidfarnsworth@gmail.com>
2017-11-23 20:24:24 -0800

    main with correct pointer notation

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aho <aho@student.42.us.org>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 00:15:08 by aho               #+#    #+#             */
-/*   Updated: 2017/11/23 20:14:25 by aho              ###   ########.fr       */
+/*   Updated: 2017/11/23 20:23:30 by cfarnswo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int				main(int argc, char **argv)
 {
 	t_tet		*tetlist;
 	int			size;
-	t_map		*map;
+	t_map		map;
 
 	if (argc != 2)
 	{
@@ -37,6 +37,6 @@ int				main(int argc, char **argv)
 	trimpieces(tetlist);
 	map.grid = makemap();
 	map.size = ft_sqrt(4 * size);
-	fillit(map, tetlist);
+	fillit(&map, tetlist);
 	return (0);
 }
