79c4718dd17bb1a07eff9b6f0b83b43f3d1a340f
chasedavid <chasedavidfarnsworth@gmail.com>
2017-11-25 21:45:50 -0800

    reworking validation checks

--- validate.c
+++ validate.c
@@ -6,12 +6,14 @@
 /*   By: aho <aho@student.42.us.org>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 12:26:45 by aho               #+#    #+#             */
-/*   Updated: 2017/11/23 21:32:05 by aho              ###   ########.fr       */
+/*   Updated: 2017/11/25 21:44:08 by cfarnswo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include <stdio.h>
 
+/*
 int			hashrightcheck(char *buf, int i)
 {
 	if (buf[i + 1] == '#')
@@ -37,6 +39,7 @@ int			hashneighborcheck(char *buf, int i, int hashes)
 {
 	if (hashes > 2)
 		return (1);
+	hashes = 0;
 	if (hashrightcheck(buf, i) == 1)
 		return (1);
 	if (hashbelowcheck(buf, i) == 1)
@@ -45,7 +48,50 @@ int			hashneighborcheck(char *buf, int i, int hashes)
 		return (1);
 	return (-1);
 }
+*/
+int			neighborhash(char *buf, int i)
+{
+	int counter;
 
+	counter = 0;
+	if (buf[i + 1] == '#' || buf[i + 5] == '#')
+	{
+		++counter;
+		if (buf[i + 1] == '#' && buf[i + 5] == '#') 
+			if (buf[i + 6] != '#')
+				++counter; 
+	}
+	return (counter);
+}
+
+int			validhashes(char *buf)
+{
+	int		i;
+	int		hashes;
+	int		counter;
+
+	i = 0;
+	hashes = 0;
+	counter = 0;
+	while (buf[i] != '\0')
+	{
+		while (hashes == 0  && buf[i] == '#')
+		{
+			++i;
+			if (buf[i] == '\0')
+				return (-1);
+		}
+		if (buf[i] == '#')
+		{
+			hashes += (((hashes + 1) > 4) ? 0 : 1);
+			counter += neighborhash(buf, i);
+		}
+		++i;
+	}
+	printf("%d:\n%d:\n", hashes, counter);
+	return ((hashes != 4 || counter != 3) ? -1 : 1);
+}
+/*
 int			validhashes(char *buf)
 {
 	int		i;
@@ -80,4 +126,4 @@ int			validhashes(char *buf)
 		i++;
 	}
 	return (1);
-}
+}*/
